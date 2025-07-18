58207d640e11217a6fafef3e030edf5c6fdfeec4
victortking-procoder <fredrickvictor01@outlook.com>
2025-07-18 12:44:28 +0100

    exploring django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,12 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
 # Create your views here.
 def list_books(request):
@@ -14,3 +20,8 @@ class LibraryDetailView(DetailView, TemplateView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+class UserRegistration(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')

997cf754686450dd17ba6b41fe798679aff7f43f
victortking-procoder <fredrickvictor01@outlook.com>
2025-07-18 09:24:20 +0100

    exploring django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book, Library
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'list_books.html', context)

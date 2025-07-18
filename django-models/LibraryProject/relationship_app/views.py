66a8cec150f2afefc2e787e657a3a4f4692d1858
victortking-procoder <fredrickvictor01@outlook.com>
2025-07-18 14:56:02 +0100

    exploring django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -26,6 +26,6 @@ def register(request):
     pass
 
 class UserRegistration(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')

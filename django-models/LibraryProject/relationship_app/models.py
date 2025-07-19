cb978d041b54eeb78cfc7c845b155fd9777c6fa8
victortking-procoder <fredrickvictor01@outlook.com>
2025-07-19 19:22:14 +0100

    exploring django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -30,13 +30,14 @@ class Librarian(models.Model):
         return self.name
 
 class UserProfile(models.Model):
-    class RoleChoices(models.TextChoices):
-        ADMIN = 'Admin', 'Admin'
-        LIBRARIAN = 'Librarian', 'Librarian'
-        MEMBER = 'Member', 'Member'
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member')
+    )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=100, choices=RoleChoices.choices)
+    role = models.CharField(max_length=100, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"

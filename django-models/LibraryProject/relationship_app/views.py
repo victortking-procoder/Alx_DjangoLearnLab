cabdd7ce837c19d406e6087f4760d7d2a6a3b780
victortking-procoder <fredrickvictor01@outlook.com>
2025-07-19 19:31:46 +0100

    exploring django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Create your views here.
 def list_books(request):
@@ -29,3 +30,31 @@ class UserRegistration(CreateView):
     form_class = UserCreationForm()
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
+
+# Role check functions
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Admin view
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')

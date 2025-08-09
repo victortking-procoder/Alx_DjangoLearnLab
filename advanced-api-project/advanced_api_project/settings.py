ed8f914c391d68eb07ee324517ecfc67a0777e58
victortking-procoder <fredrickvictor01@outlook.com>
2025-08-09 17:46:27 +0100

    implemented crud views and url routing

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'django_filters',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,11 +2,24 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author']
+
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['title', 'publication_year']
+
 
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()

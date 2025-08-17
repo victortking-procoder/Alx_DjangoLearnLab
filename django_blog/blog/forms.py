c2aa8ca6a6e996d42daec16a2702df3f03a4e996
victortking-procoder <fredrickvictor01@outlook.com>
2025-08-17 18:44:11 +0100

    implemented tagging and search functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "tags"]  # author is set automatically
+        widgets = {
+            "tags": TagWidget(),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:

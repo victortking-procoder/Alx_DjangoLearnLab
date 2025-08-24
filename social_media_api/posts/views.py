b07d7236ea78f16bb0613d4b311ff9b218b6fd86
victortking-procoder <fredrickvictor01@outlook.com>
2025-08-24 21:42:36 +0100

    implemented notifications and like functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -75,7 +75,7 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
 
         # Prevent multiple likes
         like, created = Like.objects.get_or_create(user=request.user, post=post)
@@ -99,7 +99,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post).first()
 
         if like:
